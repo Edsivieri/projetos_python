071e20828bf508dad576b0fc1a9912ecc0ad2b28
eduardo <eduardorochavieira2@gmail.com>
2025-08-20 15:26:19 -0300

    testes t

---
+++ exerc/exercicio.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        salario = float(input("digite seu salário: "))
+        salario isinstance 
+        print(salario)
+        break    
+    except ValueError:
+        print("Número digitado incorretamente ") 
+        continue
