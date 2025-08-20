a9b7ab5e485d508cfa06e9ff8b90c283662f256c
eduardo <eduardorochavieira2@gmail.com>
2025-08-20 15:34:50 -0300

    testett

---
+++ exerc/main.py
@@ -0,0 +1,12 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+soma = num1 + num2
+print(soma)
+
+divisão = num1//num2
+print(divisão)
+
+quadrado = num1**2
+
+print(quadrado)
