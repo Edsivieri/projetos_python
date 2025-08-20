f14e334be302589ca3236da4cc17c82460ed8c1b
eduardo <eduardorochavieira2@gmail.com>
2025-08-20 15:52:02 -0300

    teste 3

--- exerc/exercicio.py
+++ exerc/exercicio.py
@@ -1,9 +1,22 @@
+# Isso é um comentário
 while True:
     try:
         salario = float(input("digite seu salário: "))
-        salario isinstance 
         print(salario)
+        break
+    except ValueError:
+        print("Número digitado incorretamente ") 
+        continue
+
+while True:
+    try:
+        bonus = float(input("digite seu bonus: "))
+        print(bonus)
         break    
     except ValueError:
         print("Número digitado incorretamente ") 
         continue
+
+
+KPI = salario + bonus
+
